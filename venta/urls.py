885acb6236e5900bfb3898f75a872b043a5c9ef2
Ariel Martinez <aminstalaciones@yahoo.com.ar>
2023-07-06 22:11:58 -0300

    cambios!!!

--- venta/urls.py
+++ venta/urls.py
@@ -10,7 +10,7 @@ from .views import AutoContacto
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", AutoView.as_view(), name = "inicio"),
-    path("", AutoAcerca.as_view(), name = "acerca"),
-    path("", AutoCatalogo.as_view(), name = "catalogo"),
-    path("", AutoContacto.as_view(), name = "contacto"),
+    path("acerca/", AutoAcerca.as_view(), name="acerca"),
+    path("catalogo/", AutoCatalogo.as_view(), name="catalogo"),
+    path("contacto/", AutoContacto.as_view(), name="contacto"),
 ]
