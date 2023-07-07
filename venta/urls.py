43860559aa9a8409f4d65231eaf1f04119d0e26e
Ariel Martinez <aminstalaciones@yahoo.com.ar>
2023-07-06 21:03:17 -0300

    modificacion de carpetas

--- venta/urls.py
+++ venta/urls.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import AutoView
+from .views import AutoAcerca
+from .views import AutoCatalogo
+from .views import AutoContacto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", AutoView.as_view(), name = "inicio"),
+    path("", AutoAcerca.as_view(), name = "acerca"),
+    path("", AutoCatalogo.as_view(), name = "catalogo"),
+    path("", AutoContacto.as_view(), name = "contacto"),
 ]
--- venta/views.py
+++ venta/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class AutoView(TemplateView):
     template_name = "index.html"
+class AutoAcerca(TemplateView):
+    template_name = "acerca.html"
+class AutoCatalogo(TemplateView):
+    template_name = "catalogo.html"
+class AutoContacto(TemplateView):
+    template_name = "contacto.html"
