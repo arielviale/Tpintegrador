7f63ae3bfa3824f7dafe683e8e6e5e066655ba82
Hugo Ariel Martinez <aminstalaciones@yahoo.com.ar>
2023-07-14 12:21:00 +0000

    Tratando de hacer andar el crud.

--- venta/urls.py
+++ venta/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path , include
-from .views import AutoAcerca , AutoCatalogo ,AutoContacto, AutoView
+from django.urls   import path , include
+from .views        import AutoAcerca , AutoCatalogo ,AutoContacto, AutoView
 
 
 urlpatterns = [
